0ed0af2ab2f50a40ba88226485913de3e4e6b5c2
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-02-02 06:51:48 +0300

    Login controller eklendi.

---
+++ MVC-Authorize/MVC-Authorize/Controllers/SecurityController.cs
@@ -0,0 +1,45 @@
+﻿using MVC_Authorize.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_Authorize.Controllers
+{
+   
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        devrimme_ramazanBeyDbEntities db=new devrimme_ramazanBeyDbEntities();
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Tbl_User user)
+        {
+            var _user = db.Tbl_User.FirstOrDefault(x=>x.Username==user.Username && x.Password==user.Password);
+            if (_user!=null)
+            {
+                FormsAuthentication.SetAuthCookie(_user.Username,false);
+                return RedirectToAction("Index","Departments");
+            }else
+            {
+                ViewBag.Message = "Geçersiz Kullanıcı Adı veya Parola";
+                return View();
+            }
+
+          
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
